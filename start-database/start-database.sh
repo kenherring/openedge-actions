053da4e59d6245efd37f978e92c75953f53b74ac
kenherring <kennethherring@gmail.com>
2025-11-30 10:50:18 -0500

    start database

--- start-database/start-database.sh
+++ start-database/start-database.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 echo 'starting database...'
 
 DB_PARAMS="$OPT_PATH -S $OPT_PORT -minport $OPT_MINPORT -maxport $OPT_MAXPORT $OPT_ADDITIONAL_PARAMETERS"
+echo "proserve $DB_PARAMS"
 
 # shellcheck disable=SC2086
 if proserve $DB_PARAMS; then
