e9d099f246d24a2832dbe364f748c5852f7de191
kenherring <kennethherring@gmail.com>
2025-11-30 10:36:10 -0500

    start database

---
+++ start-database/start-database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+echo 'starting database...'
+
+DB_PARAMS="$OPT_PATH -S $OPT_PORT -minport $OPT_MINPORT -maxport $OPT_MAXPORT $OPT_ADDITIONAL_PARAMETERS"
+
+# shellcheck disable=SC2086
+if proserve $DB_PARAMS; then
+    EXIT_CODE=0
+else
+    EXIT_CODE=$?
+fi
+echo "EXIT_CODE=$EXIT_CODE"
+
+echo "database started on port $OPT_PORT"
