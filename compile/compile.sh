8ee3dae9561995e81ed0fb16958099244dcb4b84
kenherring <kennethherring@gmail.com>
2025-12-04 04:22:26 -0500

    compile

---
+++ compile/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+EXIT_CODE=0
+if ! ant compile -f '${{ github.action_path }}/build.xml' -Dbasedir="$(pwd)" | tee "$RUNNER_TEMP/compile.log"; then
+    EXIT_CODE=$?
+fi
+echo "EXIT_CODE=$EXIT_CODE"
+cat "$RUNNER_TEMP/compile.log"
+
+grep "\[PCTCompile\] [0-9]* file(s) compiled" "$RUNNER_TEMP/compile.log"
+grep "\[PCTCompile\] Failed to compile *[0-9]* *file(s)" "$RUNNER_TEMP/compile.log" || true
+
+FILES_COMPILED=$(grep "\[PCTCompile\] [0-9]* file(s) compiled" "$RUNNER_TEMP/compile.log" | tail -1 | cut -d' ' -f2)
+echo "FILES_COMPILED=$FILES_COMPILED"
+
+COMPILE_ERRORS=$(grep "\[PCTCompile\] Failed to compile *[0-9]* *file(s)" "$RUNNER_TEMP/compile.log" || echo 0 | tail -1 | cut -d' ' -f5 )
+echo "COMPILE_ERRORS=$COMPILE_ERRORS"
+
+echo "files-compiled=$FILES_COMPILED" >> "$GITHUB_OUTPUT"
+echo "compile-errors=$COMPILE_ERRORS" >> "$GITHUB_OUTPUT"
