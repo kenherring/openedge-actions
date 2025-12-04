f1c11c9671d4c593f8f11b44d1b81068f535aa22
kenherring <kennethherring@gmail.com>
2025-12-04 03:59:14 -0500

    compile

--- setup/setup_abl.sh
+++ setup/setup_abl.sh
@@ -40,11 +40,13 @@ check-existing-dlc () {
 
 copy-dlc-from-container () {
     check-existing-dlc && return 0
-    echo "::notice file=$0::Copying DLC contents from container"
+
+    echo "::group::Copying DLC contents from container"
     ## copy DLC from docker container
     docker run --name setup_abl "progresssoftware/prgs-oedb:${ABL_VERSION}_ent" bash -c 'exit 0'
     docker cp setup_abl:/psc/dlc/. "$DLC"
     docker rm setup_abl >/dev/null
+    echo "::groupend::"
 }
 
 create-configuration () {
