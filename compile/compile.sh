abfa37009b20675cda35c976770255c70efcfab0
kenherring <kennethherring@gmail.com>
2025-12-04 04:26:12 -0500

    compile

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-set -x
 
 EXIT_CODE=0
 if ! ant compile -f '${{ github.action_path }}/build.xml' -Dbasedir="$(pwd)" | tee "$RUNNER_TEMP/compile.log"; then
     EXIT_CODE=$?
 fi
 echo "EXIT_CODE=$EXIT_CODE"
-cat "$RUNNER_TEMP/compile.log"
+cat "$RUNNER_TEMP/compile.log" || true
 
-grep "\[PCTCompile\] [0-9]* file(s) compiled" "$RUNNER_TEMP/compile.log"
+grep "\[PCTCompile\] [0-9]* file(s) compiled" "$RUNNER_TEMP/compile.log" || true
 grep "\[PCTCompile\] Failed to compile *[0-9]* *file(s)" "$RUNNER_TEMP/compile.log" || true
 
 FILES_COMPILED=$(grep "\[PCTCompile\] [0-9]* file(s) compiled" "$RUNNER_TEMP/compile.log" | tail -1 | cut -d' ' -f2)
@@ -20,3 +19,8 @@ echo "COMPILE_ERRORS=$COMPILE_ERRORS"
 
 echo "files-compiled=$FILES_COMPILED" >> "$GITHUB_OUTPUT"
 echo "compile-errors=$COMPILE_ERRORS" >> "$GITHUB_OUTPUT"
+
+if [ "$EXIT_CODE" != "0" ]; then
+    echo "::error file=$0::ant compile failed (EXIT_CODE=$EXIT_CODE)"
+    exit "$EXIT_CODE"
+fi
