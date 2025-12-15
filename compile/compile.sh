21d957b0798fdd9cf48b9425ff3e8f96f5591be0
kenherring <kennethherring@gmail.com>
2025-12-15 03:41:29 -0500

    compile failure test

--- compile/compile.sh
+++ compile/compile.sh
@@ -33,23 +33,17 @@ done
 [ -z "${PROPATH_ENTRY_9:-}" ] && PROPATH_ENTRY_9=$PROPATH_ENTRY_1
 export PROPATH_ENTRY_1 PROPATH_ENTRY_2 PROPATH_ENTRY_3 PROPATH_ENTRY_4 PROPATH_ENTRY_5 PROPATH_ENTRY_6 PROPATH_ENTRY_7 PROPATH_ENTRY_8 PROPATH_ENTRY_9
 
-EXIT_CODE=0
 if ant compile -f "$GITHUB_ACTION_PATH/build.xml" -Dbasedir="$(pwd)" | tee "$RUNNER_TEMP/compile.log"; then
     EXIT_CODE=$?
-    echo "COMPILE SUCCESS (EXIT_CODE=$EXIT_CODE)"
 else
     EXIT_CODE=$?
-    echo "COMPILE FAILED (EXIT_CODE=$EXIT_CODE)"
 fi
 
 FILES_COMPILED=$(grep "\[PCTCompile\] [0-9]* file(s) compiled" "$RUNNER_TEMP/compile.log" | tail -1 | cut -d' ' -f2)
-echo "files-compiled=$FILES_COMPILED"
 echo "files-compiled=$FILES_COMPILED" >> "$GITHUB_OUTPUT"
 COMPILE_ERRORS=$(grep "\[PCTCompile\] Failed to compile *[0-9]* *file(s)" "$RUNNER_TEMP/compile.log" | tail -1 | cut -d' ' -f6 || echo 0)
-echo "compile-errors=$COMPILE_ERRORS"
 echo "compile-errors=$COMPILE_ERRORS" >> "$GITHUB_OUTPUT"
 
-echo "EXIT_CODE=$EXIT_CODE"
 if [ "$EXIT_CODE" != "0" ]; then
     echo "::error file=$0::ant compile failed (EXIT_CODE=$EXIT_CODE)"
     exit "$EXIT_CODE"
