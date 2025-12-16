7e30104d4fc641e4c10bf2cab5ab1254461f59f1
kenherring <kennethherring@gmail.com>
2025-12-15 21:11:30 -0500

    debug post step

--- with-post-step/main.js
+++ with-post-step/main.js
@@ -1,20 +1,27 @@
 // Ref: https://github.com/pyTooling/Actions/blob/main/with-post-step/main.js
-const { spawn } = require("child_process");
-const { appendFileSync } = require("fs");
-const { EOL } = require("os");
+const { spawn } = require("child_process")
+const { appendFileSync } = require("fs")
+const { EOL } = require("os")
 
 function run(cmd) {
-  const subprocess = spawn(cmd, { stdio: "inherit", shell: true });
+  console.log('starting cmd: ' + cmd)
+  const subprocess = spawn(cmd, { stdio: "inherit", shell: true })
   subprocess.on("exit", (exitCode) => {
-    process.exitCode = exitCode;
-  });
+    process.exitCode = exitCode
+  })
 }
 
-const key = process.env.INPUT_KEY.toUpperCase();
+const key = process.env.INPUT_KEY.toUpperCase()
 
 if ( process.env[`STATE_${key}`] !== undefined ) { // Are we in the 'post' step?
-  run(process.env.INPUT_POST);
+  console.log('WE ARE IN THE POST STEP')
+  run(process.env.INPUT_POST)
 } else { // Otherwise, this is the main step
-  appendFileSync(process.env.GITHUB_STATE, `${key}=true${EOL}`);
-  run(process.env.INPUT_MAIN);
+  console.log('WE ARE IN THE MAIN STEP (process.env.INPUT_MAIN=' + process.env.INPUT_MAIN + ')')
+  if (process.env.INPUT_MAIN) {
+    appendFileSync(process.env.GITHUB_STATE, `${key}=true${EOL}`)
+    run(process.env.INPUT_MAIN)
+  } else {
+    console.log('skipping... INPUT_MAIN=undefined')
+  }
 }
